b0804212317d340602d773ca1ce1fe605a97ae5f
Hordienkoihor <hordienkoihor05@gmail.com>
2026-03-12 15:10:37 +0200

    tcp

--- shpp_level_2/2_3/dist/tcp/index.js
+++ shpp_level_2/2_3/dist/tcp/index.js
@@ -2,6 +2,16 @@ import * as net from 'net';
 let server = net.createServer(function (socket) {
     socket.write('Success connection\r\n');
     socket.pipe(socket);
+    const ip = socket.remoteAddress;
+    const time = Date.now();
+    console.log('ip: ' + ip);
+    console.log('time: ' + time);
+    socket.on('end', (socket) => {
+        console.log('close initialised');
+    });
+    socket.on('close', (socket) => {
+        console.log('connection closed');
+    });
 });
 server.listen(1337, '127.0.0.1');
 //# sourceMappingURL=index.js.map
--- shpp_level_2/2_3/tcp/index.ts
+++ shpp_level_2/2_3/tcp/index.ts
@@ -3,6 +3,20 @@ import * as net from 'net';
 let server = net.createServer(function(socket) {
     socket.write('Success connection\r\n');
     socket.pipe(socket);
+
+    const ip = socket.remoteAddress
+    const time = Date.now();
+
+    console.log('ip: ' + ip);
+    console.log('time: ' + time);
+
+    socket.on('end', (socket) => {
+        console.log('close initialised')
+    })
+
+    socket.on('close', (socket) => {
+        console.log('connection closed')
+    })
 });
 
 server.listen(1337, '127.0.0.1');
